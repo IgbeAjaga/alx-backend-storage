7b3af4407a27c5cf8c999efc28e20d80492eebaf
IgbeAjaga <ajagaigbe144@gmail.com>
2023-12-14 23:18:02 +0000

    advanced sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,8 @@
 -- Create a script to list Glam rock bands ranked by longevity
 SELECT 
     band_name, 
-    (IFNULL(split, '2020') - formed) AS lifespan
+    IF(formed IS NOT NULL AND split IS NOT NULL, 2022 - LEAST(formed, split), 2022 - GREATEST(formed, split)) AS lifespan
 FROM metal_bands
 WHERE style = 'Glam rock'
 ORDER BY lifespan DESC;
+
