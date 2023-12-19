a6b7ad03b6f3a156073d19adbbb60c5ba9679a44
IgbeAjaga <ajagaigbe144@gmail.com>
2023-12-19 11:04:36 -0800

    all MongoDB

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -4,32 +4,26 @@ all students sorted by average score
 """
 
 
-from pymongo import MongoClient
-
 def top_students(mongo_collection):
-    """function that returns all students sorted by average score
+    """returns all students in a collection sorted by average score.
     """
-    students = mongo_collection.find()
-
-    for student in students:
-        scores = [topic['score'] for topic in student['topics']]
-        average_score = sum(scores) / len(scores) if scores else 0
-        student['averageScore'] = round(average_score, 2)
-
-    return sorted(students, key=lambda x: x['averageScore'], reverse=True)
-
-# Test the function with the provided script
-if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    students_collection = client.my_db.students
-
-    # ... (Your insertion of data code here)
-
-    students = list_all(students_collection)
-    for student in students:
-        print("[{}] {} - {}".format(student.get('_id'), student.get('name'), student.get('topics')))
-
-    top_students_list = top_students(students_collection)
-    for student in top_students_list:
-        print("[{}] {} => {}".format(student.get('_id'), student.get('name'), student.get('averageScore')))
-
+    students = mongo_collection.aggregate(
+        [
+            {
+                '$project': {
+                    '_id': 1,
+                    'name': 1,
+                    'averageScore': {
+                        '$avg': {
+                            '$avg': '$topics.score',
+                        },
+                    },
+                    'topics': 1,
+                },
+            },
+            {
+                '$sort': {'averageScore': -1},
+            },
+        ]
+    )
+    return students
