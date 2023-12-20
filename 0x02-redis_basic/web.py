37ce51e7aa448fac830cc63cc30c38175e89add4
IgbeAjaga <ajagaigbe144@gmail.com>
2023-12-20 21:40:33 +0000

    Implementing an expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,36 +1,32 @@
 #!/usr/bin/env python3
-"""A module for request caching and tracking tools.
+"""Module for implementing an expiring web cache and tracker.
 """
 import redis
 import requests
-from functools import wraps
-from typing import Callable
 
 redis_instance = redis.Redis()
-"""The module-level Redis instance.
+"""Module-level Redis instance for caching.
 """
 
-def data_cacher(method: Callable) -> Callable:
-    """Caches the output of fetched data.
+def cache_expiration(method):
+    """Decorator for caching the output of a fetched web page
+    with expiration time.
     """
-    @wraps(method)
-    def invoker(url) -> str:
+    def wrapper(url):
         """Wrapper function for caching the output.
         """
         redis_instance.incr(f'count:{url}')
         result = redis_instance.get(f'result:{url}')
         if result:
             return result.decode('utf-8')
-        result = method(url)
+        response = method(url)
         redis_instance.set(f'count:{url}', 0)
-        redis_instance.setex(f'result:{url}', 10, result)
-        return result
-    return invoker
+        redis_instance.setex(f'result:{url}', 10, response.text)
+        return response.text
+    return wrapper
 
-@data_cacher
-def get_page(url: str) -> str:
-    """Returns the content of a URL after caching the request's response
-    and tracking the request.
+@cache_expiration
+def get_page(url):
+    """Function to obtain the HTML content of a URL and return it.
     """
-    return requests.get(url).text
-
+    return requests.get(url)
