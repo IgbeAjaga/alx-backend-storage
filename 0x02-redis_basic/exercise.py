ef9fa7cfb096777330cad94bc6c62e84e48aeab1
IgbeAjaga <ajagaigbe144@gmail.com>
2023-12-20 21:04:24 +0000

    Writing strings to Redis

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+A script for writing strings to Redis.
+"""
+import redis
+import uuid
+
+class Cache:
+    def __init__(self):
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data) -> str:
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
+
+if __name__ == '__main__':
+    cache = Cache()
+    data = b"hello"
+    key = cache.store(data)
+    print(key)
+
+    local_redis = redis.Redis()
+    print(local_redis.get(key))
